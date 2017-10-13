57ff88bb8b36eb5122d216f5f9dab8f969ef075c
Keegan Lowenstein <keegan@bugsnag.com>
2017-10-13 16:53:41 -0700

    delay scroll

--- better-confluence-links.js
+++ better-confluence-links.js
@@ -7,9 +7,9 @@ $(function() {
     return;
   }
 
-  var ref = document.referrer;
+  let ref = document.referrer;
 
-  var jiraUrlKeyMatchers = [
+  let jiraUrlKeyMatchers = [
     // standard full page issue view
     /\/browse\/([A-Z]+-\d+)/,
 
@@ -21,9 +21,9 @@ $(function() {
 
   // Scroll to the first occurrence of the link to the referring jira issue
   // in the confluence doc
-  var scollToIssueIdInConfluence = function(issueId) {
-    var firstLink, headerHeight;
-    var issueLinks = $(".jira-issue[data-jira-key='" + issueId + "']");
+  let scollToIssueIdInConfluence = function(issueId) {
+    let firstLink, headerHeight;
+    let issueLinks = $(`.jira-issue[data-jira-key="${issueId}"]`);
 
     if (issueLinks.length > 0) {
       firstLink = $(issueLinks[0]);
@@ -37,10 +37,15 @@ $(function() {
   };
 
   $.each(jiraUrlKeyMatchers, function(i, jiraIssueIdRegex) {
-    var issueId;
     if (ref.match(jiraIssueIdRegex)) {
-      issueId = ref.match(jiraIssueIdRegex)[1];
-      scollToIssueIdInConfluence(issueId);
+      let issueId = ref.match(jiraIssueIdRegex)[1];
+
+      // confluence loads the jira links with a ajax request (at least in some
+      // cases) so doc ready does not necessarily mean jira links are in the
+      // dom
+      setTimeout(() => {
+        scollToIssueIdInConfluence(issueId);
+      }, 1000);
     }
   });
 })
